e88086cba28bcf5141de97b6561322d8c46d9304
Oleg Kozlovsky <okozlovsky@gmail.com>
2019-03-14 22:02:59 +0300

    Metadata updated for v0.2.8

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("(c) Garwel 2017-2018")]
+[assembly: AssemblyCopyright("(c) Garwel 2017-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
